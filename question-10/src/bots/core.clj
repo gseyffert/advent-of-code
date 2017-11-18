eeabb784ca6b78594d34d89ad599f2b50a58eabc
Graham Seyffert <graham.seyffert@capitalone.com>
2017-11-18 14:38:07 -0800

    Commands parsed.

--- question-10/src/bots/core.clj
+++ question-10/src/bots/core.clj
@@ -1,32 +1,47 @@
 (ns bots.core
   (:gen-class)
-  (:require [clojure.string :as string]))
+  (:require
+    [clojure.string :as string]
+    [clojure.java.io :as io]))
 
 (def bot-mappings {})
 
-; (defn )
+(defn make-keyword [& strs]
+  (keyword (apply str strs)))
 
-(defn not-ten [int]
-  (not= 10 int))
+(defn make-bot-key [num]
+  (make-keyword "bot" num))
 
-(defn split-return [string]
-  (string/split string #"\r|\n"))
+(defn get-nums [string]
+  (re-seq #"\d+" string))
 
-(defn print [string]
-  (println "printing" string))
+(defn parse-initial-value [[value bot-num]]
+  [:give (make-bot-key bot-num) (Integer/parseInt value 10)])
+
+(defn parse-give-to [[from-bot low high] [_ give-low-to give-high-to]]
+  [:from
+   (make-bot-key from-bot)
+   (make-keyword give-low-to low)
+   (make-keyword give-high-to high)])
+
+(defn parse-instruction [instruction]
+  (let [nums (get-nums instruction)]
+    (if (= 2 (count nums))
+      (parse-initial-value nums)
+      (parse-give-to nums (re-seq #"bot|output" instruction)))))
+
+; (defn init-state [])
 
 (defn -main
   [& args]
-  (let [file         (if (not (empty? args)) (first args) "./input.txt")
-        ; This file uses \r for \return characters for some reason,
-        ; so string/split-lines doesn't work
-        instructions (map str (split-return (slurp file)))
-        starting     (filter #(string/starts-with? "value" %) instructions)
-        hand-offs    (filter #(string/starts-with? "bot" %) instructions)
+  (let [file         (if (not (empty? args)) (first args) "./resources/input.txt")
+        instructions (string/split-lines (string/trim (slurp file)))
+        parsed       (map parse-instruction instructions)
+        initial      (filter #(= :give (first %)) parsed)
+        transitions  (filter #(= :from (first %)) parsed)
        ]
-    ; (map print)
-    (println (map print instructions))
-    (println starting)
-    (println hand-offs)
+    (println initial)
+    (println transitions)
+    ; (->> parsed)
     ))
 
